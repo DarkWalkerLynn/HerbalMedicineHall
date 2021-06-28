2730d78eb84008885744b57d6a58313f7cb166e9
DarkWalkerLynn <70731361+DarkWalkerLynn@users.noreply.github.com>
2021-06-28 08:35:04 +0800

    Add files via upload

---
+++ HerbalMedicineHall1.0/App.vue
@@ -0,0 +1,24 @@
+<script>
+	export default {
+		globalData:{
+			isSigned:false,
+			nickname:'',
+			
+		},
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import "uview-ui/theme.scss";
+	@import "data/mycss.css";
+</style>
